781d8ac9aef2f256791b761b7ccfe072453013d7
rollingrover <relborg@outlook.com>
2026-07-15 23:34:59 +0200

    updated partner details

--- src/data/business.ts
+++ src/data/business.ts
@@ -64,7 +64,7 @@ export const business = {
    * Per final audit: all hrefs as provided, vertical list in footer.
    */
   partners: [
-    { name: 'Diza Kwa Smolo', href: 'https://www.dizakwasmolo.co.za' },
+    { name: 'Diza Kwa-Smolo Community Uplifting Initiative', href: 'https://www.dizakwasmolo.co.za' },
     { name: 'Zatours', href: 'https://zatours.co.za' },
     { name: 'Mzamo Cultural Village & Homestead', href: 'https://www.mzamovillagehomestead.co.za' },
     { name: 'eThlathini Rest Camp', href: 'https://www.ethlathini.co.za' },
