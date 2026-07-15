c2434054996c55a371388a00567f385ca4c23ebd
rollingrover <relborg@outlook.com>
2026-07-15 09:05:02 +0200

    add google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Site Verification */}
+        <meta name="google-site-verification" content="Va3hG3ieJYtJdYVNOgd1mJOgxobGAL2CryhESUImqOo" />
+      </head>
       <body>
         {children}
         {/* Vercel Analytics — tracks page views automatically */}
         <Analytics />
-        </body>
+      </body>
     </html>
   );
 }
